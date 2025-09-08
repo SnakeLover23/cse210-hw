40f7075c3490bd3d118d41bc0f3857fdc7891a1d
SnakeLover23 <snakey232323@gmail.com>
2025-09-08 12:30:20 -0600

    finnished exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Give me a grade nummber. (0 - 100) ");
+        string grade = Console.ReadLine();
+        int intgrade = int.Parse(grade);
+
+        string lettergrade;
+
+        if (intgrade >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (intgrade >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (intgrade >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (intgrade >= 60)
+        {
+            lettergrade = "D";
+        }
+        else if (intgrade < 0)
+        {
+            lettergrade = "Inv";
+        }
+        else if (intgrade < 60)
+        {
+            lettergrade = "F";
+        }
+        else
+        {
+            lettergrade = "Inv";
+        }
+
+        Console.WriteLine($"Your grade is {lettergrade}.");
+
+        if (intgrade >= 70)
+        {
+            Console.WriteLine("Congradulations, you pass the class!");
+        }
+        else
+        {
+            Console.WriteLine("You need to do better.");
+        }
     }
 }
