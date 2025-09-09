3c15f55bb70abe168f279e8267836a895d807b72
SnakeLover23 <snakey232323@gmail.com>
2025-09-09 16:51:40 -0600

    finnished exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finnished");
+        List<float> numbers = new List<float>();
+        bool end = false;
+        float largest = 0;
+        float total = 0;
+        float average = 0;
+        int count = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            string inpt = Console.ReadLine();
+            float number = float.Parse(inpt);
+            if (number > largest)
+            {
+                largest = number;
+            }
+            if (number == 0)
+            {
+                end = true;
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+        } while (end == false);
+        foreach (float i in numbers)
+        {
+            total = total + i;
+            count = count + 1;
+        }
+        average = total / count;
+        Console.WriteLine($"The total is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
